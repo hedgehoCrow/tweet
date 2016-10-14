7fb15f2958933b58884a029c16d50bd18b975ba0
CrowCrowzard <crowizard@gmail.com>
2016-10-14 15:22:20 +0900

    add humidity data

--- weather_tweet.py
+++ weather_tweet.py
@@ -12,6 +12,8 @@ status = get_status()
 temp = str(get_temp())
 temp_max = str(get_temp_max())
 temp_min = str(get_temp_min())
+humidity = str(get_humidity())
 
-params = {"status": "test\n滋賀の天気は" + status + "です\n" + "現在の気温は" + temp + "度\n" + "今日の最高気温は" + temp_max + "度\n" + "今日の最低気温は" + temp_min + "度です\n"}
+params = {"status": "test\n滋賀の天気は" + status + "です\n" + "現在の気温は" + temp + "度\n" + "湿度は" + humidity + "です\n"}
+#params = {"status": "test\n滋賀の天気は" + status + "です\n" + "現在の気温は" + temp + "度\n" + "今日の最高気温は" + temp_max + "度\n" + "今日の最低気温は" + temp_min + "度です\n"}
 req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
