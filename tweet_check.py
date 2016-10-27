e1e92d5d4c2e07279609f321cab37efae35062da
CrowCrowzard <crowizard@gmail.com>
2016-10-27 20:59:10 +0900

    get tweet status

--- tweet_check.py
+++ tweet_check.py
@@ -1,24 +1,48 @@
 # coding: UTF-8
 
+import sys
 import tweepy
 
 import key
+import loop_destroy
 
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
-        print (status.text)
+        print ('ツイート') # 量が多すぎるのでサンプルでは関数の判定のみ
+        #print (status.text)
         return True
 
     def on_error(self, status_code):
         print ('エラー発生: ' + str(status_code))
         return True
 
+    def on_connect(self):
+        print ('接続しました')
+    
+    def on_disconnect(self, notice): 
+        print ('接続されました:' + str(notice.code))
+        
+    def on_limit(self, track):
+        print ('受信リミットが発生しました:' + str(track))
+
+    def on_timeout(self):
+        print ('タイムアウト')
+
+    def on_warning(self, notice):
+        print ('警告メッセージ:' + str(notice.message))
+
+    def on_exception(self, exception):
+        print ('例外エラー:' + str(exception))
+        
 auth = tweepy.OAuthHandler(key.CK, key.CS)
 auth.set_access_token(key.AK, key.AS)
 
-listenr = Listener()
-stream = tweepy.Stream(auth, listenr)
-
-stream.sample()     # Twitter全ツイートから1%ピックアップ
-#stream.userstream()# ユーザ自信のTL
+while True:
+    try:
+        listenr = Listener()
+        stream = tweepy.Stream(auth, listenr)
+        stream.sample()     # Twitter全ツイートから1%ピックアップ
+        #stream.userstream()# ユーザ自信のTL
+    except:
+        sys.exit(0)
 
